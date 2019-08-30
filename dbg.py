64eddb6e8b326394cd8e1d00c9ced8819c1a21fe
Stephan Gocht <gocht@kth.se>
2019-08-30 17:03:59 +0200

    Fixing comment.

--- dbg.py
+++ dbg.py
@@ -1,7 +1,11 @@
 import py
 import sys
 
-# to debug cpp code you can run gdb python3
-# > r dbg.py instance.opb proof.log
+# to debug cpp code with specific instance you can run
+# > gdb python3
+# > r ./refpy/ instance.opb proof.log
+# to debug test cases run
+# > gdb python3
+# r dbg.py ./tests/
 
 py.test.cmdline.main(sys.argv)

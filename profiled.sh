2c4e94fde0b453e0c269b8ab65297fed7a9c7d31
Stephan Gocht <gocht@kth.se>
2019-09-18 12:08:49 +0200

    Switch profiling.

--- profiled.sh
+++ profiled.sh
@@ -8,7 +8,7 @@ REFPY_DIR=`dirname ${BASH_SOURCE[0]}`
 # heap and performance
 # LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libprofiler.so:/usr/lib/x86_64-linux-gnu/libtcmalloc.so CPUPROFILE=prof.out CPUPROFILE_FREQUENCY=1000 HEAPPROFILE=./heap.prof refpy "$@"
 # performance only
-# LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libprofiler.so CPUPROFILE=prof.out CPUPROFILE_FREQUENCY=1000 refpy "$@"
+LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libprofiler.so CPUPROFILE=prof.out CPUPROFILE_FREQUENCY=1000 refpy "$@"
 # heap only
-LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc.so HEAPPROFILE=./heap.prof refpy "$@"
+# LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc.so HEAPPROFILE=./heap.prof refpy "$@"
 google-pprof --addresses --callgrind $REFPY_DIR/refpy/optimized/constraints.cpython-36m-x86_64-linux-gnu.so prof.out > prof.callgrind

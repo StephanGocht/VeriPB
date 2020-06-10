9cc1cfcdf878293921b17f0ca68accc6c95d5718
Stephan Gocht <stephan@gobro.de>
2020-06-10 14:08:03 +0200

    Cleaning up parsing.

--- veripb/utils.py
+++ veripb/utils.py
@@ -29,7 +29,12 @@ if profile:
 
 @TimedFunction.time("LoadFormula")
 def loadFormula(file, varMgr):
-    return parseOpb(file, varMgr)
+    formula = parseOpb(file, varMgr)
+    return {
+        "numVariables": formula.maxVar,
+        "constraints": formula.getConstraints(),
+        "objective": objectiveToDict(formula)
+    }
 
 def objectiveToDict(formula):
     if (formula.hasObjective):
@@ -60,20 +65,19 @@ def run(formulaFile, rulesFile, settings = None, arbitraryPrecision = False):
     try:
         if arbitraryPrecision:
             formula = OPBParser(context.ineqFactory).parse(formulaFile)
-            context.formula = formula["constraints"]
-            context.objective = formula["objective"]
         else:
             formula = loadFormula(formulaFile.name, context.ineqFactory.varNameMgr)
-            context.formula = formula.getConstraints()
-            context.objective = objectiveToDict(formula)
     except ParseError as e:
         e.fileName = formulaFile.name
         raise e
 
+    context.formula = formula["constraints"]
+    context.objective = formula["objective"]
+
     if arbitraryPrecision:
         context.propEngine = PropEngine()
     else:
-        context.propEngine = CppPropEngine(formula.maxVar)
+        context.propEngine = CppPropEngine(formula["numVariables"])
 
 
     verify = Verifier(

aa154bb0ad8b6bef71045119608391bf798da921
Stephan Gocht <stephan@gobro.de>
2021-03-12 15:43:32 +0100

    Reenable compiling of py files.

--- setup.py
+++ setup.py
@@ -26,11 +26,11 @@ ext_modules = [
         libraries=['gmp', 'gmpxx'],
         language='c++'
     ),
-    # Extension('veripb.constraints', sources=['veripb/constraints.py']),
-    # Extension('veripb.rules', sources=['veripb/rules.py']),
-    # Extension('veripb.parser', sources=['veripb/parser.py']),
-    # Extension('veripb.autoproving', sources=['veripb/autoproving.py']),
-    # Extension('veripb.rules_dominance', sources=['veripb/rules_dominance.py']),
+    Extension('veripb.constraints', sources=['veripb/constraints.py']),
+    Extension('veripb.rules', sources=['veripb/rules.py']),
+    Extension('veripb.parser', sources=['veripb/parser.py']),
+    Extension('veripb.autoproving', sources=['veripb/autoproving.py']),
+    Extension('veripb.rules_dominance', sources=['veripb/rules_dominance.py']),
 ]
 
 for e in ext_modules:
